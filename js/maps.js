c97d732a08c569fc3cc569b23d2b172ccf1f0318
mrbrunotte <mrbrunotte@gmail.com>
2019-05-31 15:20:10 +0200

    Map and js file added

--- js/maps.js
+++ js/maps.js
@@ -1,33 +1,35 @@
-var map, infoWindow;
+/*  Create map function: createMap() with geolocation
+    Map and infoWindow is an object from Google Maps API! */
+var map;
 
 function createMap() {
     var options = {
-        center: { lat: 43.654, lng: -79.383 },
-        zoom: 10
+        zoom: 15,
+        center: { lat: 40.712776, lng: -74.005974 }
     };
 
     map = new google.maps.Map(document.getElementById('map'), options);
 
-    var input = getElementById('search');
-    var searchBox = new google.maps.places.SearchBox(input);
+    var input = document.getElementById('search');
+    var searchBox = new google.maps.places.SearchBox(input);           // initialize the serch input from HTML file into a new variable
 
     map.addListener('bounds_changed', function () {
         searchBox.setBounds(map.getBounds());
-    })
+    });
 
-    var markers = [];
+    var markers = [];       // create and empty array
 
+    // Add a listener for the places changed events on the searchbox
     searchBox.addListener('places_changed', function () {
-        var places = searchBox.getPlaces();
+        var places = searchBox.getPlaces();                       // storing the search city
 
-        if (places.length === 0)
+        if (places.length == 0)
             return;
 
-        markers.forEach(function (m) { m.setMap(null); });
-        markers = [];
+        markers.forEach(function (m) { m.setMap(null); });       // To clear old search
+        markers = [];                                           // set array to empty again
 
         var bounds = new google.maps.LatLngBounds();
-
         places.forEach(function (p) {
             if (!p.geometry)
                 return;
@@ -43,6 +45,7 @@ function createMap() {
             else
                 bounds.extend(p.geometry.location);
         });
+
         map.fitBounds(bounds);
     });
 }
