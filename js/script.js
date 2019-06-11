cbcf6bf5da1f49402d3076636460a4d76e0874aa
mrbrunotte <mrbrunotte@gmail.com>
2019-06-11 19:31:15 +0200

    File cleanup

--- js/script.js
+++ js/script.js
@@ -1,134 +1,74 @@
-$(document).ready(function () {
-    // type_holder
-    // <div><label><input type="checkbox" class="types" value="bar" />bar</label></div>
+var request;            // moved the variables so that they have Global scope (local scope inside the functions)
+var service;            // Global scope means that they are can be accessed in the whole program!
+var markers = [];
+var map;
+var infoWindow;         // adds information to each marker when clicked
 
-    //Create variable for the search hotel, restaurant and bar from the checkboxes in HTML file
-    var types = ['hotel', 'restaurant', 'bar'];
-    var html = '';
-
-    $.each(types, function (index, value) {
-        var name = value.replace(/_/g, " ");
-        html += '<div><label><input type="checkbox" class="types" value="' + value + '" />' + capitalizeFirstLetter(name) + '</label></div>';
+function initMap() {
+    var center = new google.maps.LatLng(40.712776, -74.005974);
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        zoom: 13
     });
 
-    $('#type_holder').html(html);
-});
+    // Modification of the initMap function (request, service and nearbySearch)
+    request = {                 //Create a variable called request so that Google can find what we are looking for.
+        location: center,
+        radius: 8047,               // five Mile in meters from center of map
+        types: ['restaurant', 'bakery', 'bar', 'cafe', 'night-club', 'museum', 'movie-theater', 'lodging']
+    };
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+    infoWindow = new google.maps.InfoWindow();      //The googole maps InfoWindow function (InfoWindow is a constrfuctor!)
+
+    service = new google.maps.places.PlacesService(map);    // this object searches for our cafes and it is a function from google
+
+    service.nearbySearch(request, callback);    // we use the nearbySearch function for the request and we use the callback method to check that everything runs ok.
+
+    google.maps.event.addListener(map, 'rightclick', function (event) { // added a listener for RightClicks and create a new search for cafes around that location.
+        map.setCenter(event.LatLng)
+        clearResults(markers)
+
+        var request = {
+            location: event.LatLng,                     // the center is now centered aroung the "rightclick"
+            radius: 8047,
+            types: ['restaurant', 'bakery', 'bar', 'cafe', 'night-club', 'museum', 'movie-theater', 'lodging']
+        };
+        service.nearbySearch(request, callback);        // the nearbySearch functions request and makes the callback like the first time
+    })
 }
 
-function initialize() {
-    autocomplete = new google.maps.places.Autocomplete((document.getElementById('address')), {
-        types: ['(regions)'],
-    });
-
-    var pyrmont = new google.maps.LatLng(35.7465, -39.4629); //North Atlantic Ocean
-
-    map = new google.maps.Map(document.getElementById('map'),
-        {
-            center: pyrmont,
-            zoom: 3
-        });
-}
-
-function renderMap() {
-    var map;
-    var infowindow;
-    var autocomplete;
-    var countryRestrict = { 'country': 'in' };
-    var selectedTypes = [];
-    // Get the user defined values
-    var address = document.getElementById('city').value;
-
-    var radius = 20000;
-
-    // get the selected type
-    selectedTypes = [];
-    $('.types').each(function () {
-        if ($(this).is(':checked')) {
-            selectedTypes.push($(this).val());
-        }
-    });
-
-    //console.log(selectedTypes);
-
-    var geocoder = new google.maps.Geocoder();
-    var selLocLat = 0;
-    var selLocLng = 0;
-
-    geocoder.geocode({ 'address': address }, function (results, status) {
-        if (status === 'OK') {
-
-            selLocLat = results[0].geometry.location.lat();
-            selLocLng = results[0].geometry.location.lng();
-
-            var pyrmont = new google.maps.LatLng(selLocLat, selLocLng);
-
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: pyrmont,
-                zoom: 14
-            });
-
-            var request = {
-                location: pyrmont,
-                radius: radius,
-                types: selectedTypes
-            };
-
-            infowindow = new google.maps.InfoWindow();
-
-            var service = new google.maps.places.PlacesService(map);
-            service.nearbySearch(request, callback);
-        }
-        else {
-            alert('Geocode was not successful for the following reason: ' + status);
-        }
-    });
-}
-
+// create the callback function which is called from the service object: service.nearbySearch
+// The function checks that everything is ok and adds all the results in the results variable
 function callback(results, status) {
-
-    console.log(status, 'testing', results);
-
     if (status == google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
-            createMarker(results[i], results[i].icon);
+            markers.push(createMarker(results[i]));
         }
     }
 }
 
-function createMarker(place, icon) {
+// create the createMarker function that creates and place the markers on the map based on the results from the createMarker function
+function createMarker(place) {
     var placeLoc = place.geometry.location;
-
-    // Need to make a loop here to go throuhg the locations and pick the right icon!
     var marker = new google.maps.Marker({
         map: map,
+        //iconImage: 'http://maps.google.com/mapfiles/kml/pal2/icon54.png',     //Coffee mug
         position: place.geometry.location,
-        type: ['restaurant'],
-        icon: 'http://maps.google.com/mapfiles/kml/pal2/icon55.png', //fork and knive in a circle
         animation: google.maps.Animation.DROP
     });
-    /*this is for the hotels and bars!
-    
-        var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location,
-            type: ['hotel'],
-            icon: 'http://maps.google.com/mapfiles/kml/pal2/icon20.png', //fork and knive in a circle
-            animation: google.maps.Animation.DROP
-        });
-        var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location,
-            type: ['bar'],
-            icon: 'http://maps.google.com/mapfiles/kml/pal2/icon19.png', //fork and knive in a circle
-            animation: google.maps.Animation.DROP
-        });
-        */
 
-    google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent(place.name + '<br>' + place.vicinity);
-        infowindow.open(map, this);
+    google.maps.event.addListener(marker, 'click', function () { //This listener listens for "clicks" on the map
+
+        infoWindow.setContent(place.name);      // gets the name of the location of the clicked marker
+        infoWindow.open(map, this);             // opens the info on the marker that is clicked ("this" one)
     });
+    return marker;                              // returns the markers 
 }
+
+function clearResults(markers) {                // This function clears all the markers when the user "rightClicks"
+    for (var m in markers) {
+        markers[m].setMap(null)
+    }
+    markers = []                                // sets the marker array to empty everytime the user "rightClicks"
+}
+google.maps.event.addDomListener(window, 'load', initMap);
